4e9ad2e1fb3f9cfb1cc5bb482031d034bfaf441f
viandwi24 <fiandwi2404@gmail.com>
2021-06-25 12:41:57 +0700

    fix vlve

--- api/components/vlv02.js
+++ api/components/vlv02.js
@@ -32,8 +32,8 @@ export default {
       {
         name: 'Control',
         child: [
-          { type: 'button', name: 'button_start', class: 'icon green', text: '', icon: ['fas', 'play'], disable: (item.state.active || item.state.auto), onClick: (item) => { item.state.active = !item.state.active } },
-          { type: 'button', name: 'button_stop', class: 'icon red', text: '', icon: ['fas', 'stop'], disable: (!item.state.active || item.state.auto), onClick: (item) => { item.state.active = !item.state.active } }
+          { type: 'button', name: 'button_start', class: 'icon green', text: '', icon: ['fas', 'play'], disable: (item.state.open || item.state.auto), onClick: (item) => { item.state.open = !item.state.open } },
+          { type: 'button', name: 'button_stop', class: 'icon red', text: '', icon: ['fas', 'stop'], disable: (!item.state.open || item.state.auto), onClick: (item) => { item.state.open = !item.state.open } }
         ]
       },
       {
--- docs/_nuxt/static/1624556831/access-level/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/access-level", {data:[{}],fetch:{},mutations:[]});
