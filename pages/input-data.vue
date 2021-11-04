e3f1729e34bc58bc2bc34b8a748a187d524d1dff
viandwi24 <fiandwi0424@gmail.com>
2021-11-04 14:53:24 +0700

    rev: change size input-data to small

--- pages/input-data.vue
+++ pages/input-data.vue
@@ -21,65 +21,63 @@
         </div>
       </div>
     </div>
-    <div class="screen tw-relative tw-flex-1">
-      <div class="tw-bg-gray-200 tw-shadow-lg tw-rounded tw-h-full tw-mx-40 tw-my-10 tw-flex tw-flex-col tw-overflow-hidden">
-        <div style="display: flex; flex: 1 1 0%;overflow-y: auto;" class="tw-flex-col">
-          <div class="tw-flex">
-            <div class="tw-flex-1 tw-flex tw-space-x-4 tw-px-4 tw-py-4">
-              <div class="tw-relative tw-overflow-hidden tw-rounded tw-border tw-border-gray-400">
-                <div class="tw-top-1/2 tw-p-4 tw-self-center tw-inline">
-                  <font-awesome-icon :icon="['fas', 'calendar']" />
-                </div>
-                <input type="date" class="tw-text-xs tw-font-bold tw-py-2 tw-px-4 tw-outline-none">
+    <div class="screen tw-relative tw-px-40 tw-py-10 tw-bg-red-500">
+      <div class="tw-bg-gray-200 tw-shadow-lg tw-rounded tw-flex tw-flex-col tw-h-full tw-max-h-full">
+        <div class="tw-flex">
+          <div class="tw-flex-1 tw-flex tw-space-x-4 tw-px-4 tw-py-4">
+            <div class="tw-relative tw-overflow-hidden tw-rounded tw-border tw-border-gray-400">
+              <div class="tw-top-1/2 tw-p-4 tw-self-center tw-inline">
+                <font-awesome-icon :icon="['fas', 'calendar']" />
               </div>
-              <button class="tw-bg-gray-50 hover:tw-bg-gray-100 tw-text-xs tw-border tw-border-gray-400 tw-text-black tw-font-bold tw-py-2 tw-px-4 tw-rounded">
-                SENIN
-              </button>
+              <input type="date" class="tw-text-xs tw-font-bold tw-py-2 tw-px-4 tw-outline-none">
             </div>
-            <div class="tw-text-right tw-self-center tw-pr-4 tw-text-xl tw-text-blue-800 tw-font-semibold">
-              <button class="tw-bg-blue-500 hover:tw-bg-blue-700 tw-text-xs tw-text-white tw-font-bold tw-py-2 tw-px-4 tw-rounded">
-                Save
-              </button>
-            </div>
+            <button class="tw-bg-gray-50 hover:tw-bg-gray-100 tw-text-xs tw-border tw-border-gray-400 tw-text-black tw-font-bold tw-py-2 tw-px-4 tw-rounded">
+              SENIN
+            </button>
           </div>
-          <div class="tw-flex-1 tw-py-4 tw-px-6 tw-justify-center tw-bg-gray-300">
-            <table class="shadow-lg tw-w-full tw-mb-12">
-              <tr>
-                <th class="tw-bg-blue-500 tw-text-gray-100 tw-border tw-border-gray-300 tw-text-left tw-px-8 tw-py-4">
-                  NAME
-                </th>
-                <th class="tw-bg-blue-500 tw-text-gray-100 tw-border tw-border-gray-300 tw-text-left tw-px-8 tw-py-4">
-                  INLET
-                </th>
-                <th class="tw-bg-blue-500 tw-text-gray-100 tw-border tw-border-gray-300 tw-text-left tw-px-8 tw-py-4">
-                  OUTLET
-                </th>
-              </tr>
-              <tr v-for="(item, i) in items" :key="i" class="tw-bg-gray-200 hover:tw-text-gray-800 hover:tw-bg-gray-100">
-                <td class="tw-border tw-border-gray-300 tw-px-8 tw-py-4">
-                  <label for="toggle_1" class="tw-font-font tw-text-gray-800">
-                    {{ item.name }}
-                  </label>
-                </td>
-                <td class="tw-border tw-border-gray-300 tw-px-8 tw-py-4">
-                  <input
-                    v-model="item.inlet"
-                    class="tw-shadow tw-appearance-none tw-border tw-rounded tw-w-full tw-py-2 tw-px-3 tw-text-gray-700 tw-leading-tight focus:tw-outline-none focus:tw-shadow-outline"
-                    type="text"
-                    placeholder="Flow value..."
-                  >
-                </td>
-                <td class="tw-border tw-border-gray-300 tw-px-8 tw-py-4">
-                  <input
-                    v-model="item.outlet"
-                    class="tw-shadow tw-appearance-none tw-border tw-rounded tw-w-full tw-py-2 tw-px-3 tw-text-gray-700 tw-leading-tight focus:tw-outline-none focus:tw-shadow-outline"
-                    type="text"
-                    placeholder="Totalizer value..."
-                  >
-                </td>
-              </tr>
-            </table>
+          <div class="tw-text-right tw-self-center tw-pr-4 tw-text-xl tw-text-blue-800 tw-font-semibold">
+            <button class="tw-bg-blue-500 hover:tw-bg-blue-700 tw-text-xs tw-text-white tw-font-bold tw-py-2 tw-px-4 tw-rounded">
+              Save
+            </button>
           </div>
+        </div>
+        <div class="tw-flex-1 tw-max-h-full tw-py-4 tw-px-6 tw-bg-gray-300 tw-overflow-y-scroll" style="max-height: 100%;">
+          <table class="shadow-lg tw-w-full">
+            <tr class="tw-text-sm" width="30%">
+              <th class="tw-bg-blue-500 tw-text-gray-100 tw-border tw-border-gray-300 tw-text-left tw-px-4 tw-py-2">
+                NAME
+              </th>
+              <th class="tw-bg-blue-500 tw-text-gray-100 tw-border tw-border-gray-300 tw-text-left tw-px-4 tw-py-2">
+                INLET
+              </th>
+              <th class="tw-bg-blue-500 tw-text-gray-100 tw-border tw-border-gray-300 tw-text-left tw-px-4 tw-py-2">
+                OUTLET
+              </th>
+            </tr>
+            <tr v-for="(item, i) in items" :key="i" class="tw-bg-gray-200 hover:tw-text-gray-800 hover:tw-bg-gray-100">
+              <td class="tw-border tw-border-gray-300 tw-px-4 tw-py-2">
+                <label for="toggle_1" class="tw-text-sm tw-font-font tw-text-gray-800">
+                  {{ item.name }}
+                </label>
+              </td>
+              <td class="tw-border tw-border-gray-300 tw-px-4 tw-py-2">
+                <input
+                  v-model="item.inlet"
+                  class="tw-text-xs tw-shadow tw-appearance-none tw-border tw-rounded tw-w-full tw-py-2 tw-px-3 tw-text-gray-700 tw-leading-tight focus:tw-outline-none focus:tw-shadow-outline"
+                  type="text"
+                  placeholder="Flow value..."
+                >
+              </td>
+              <td class="tw-border tw-border-gray-300 tw-px-4 tw-py-2">
+                <input
+                  v-model="item.outlet"
+                  class="tw-text-xs tw-shadow tw-appearance-none tw-border tw-rounded tw-w-full tw-py-2 tw-px-3 tw-text-gray-700 tw-leading-tight focus:tw-outline-none focus:tw-shadow-outline"
+                  type="text"
+                  placeholder="Totalizer value..."
+                >
+              </td>
+            </tr>
+          </table>
         </div>
       </div>
     </div>
